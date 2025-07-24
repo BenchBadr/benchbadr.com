17356d522eff1ab182c3a48f0ee067c9e5b9beb2
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2025-07-24 22:37:56 +0200

    re init

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import ThemeProvider, {ThemeContext} from './ctx/ThemeContext';
+import SidebarProvider from './ctx/SidebarContext';
+import { useContext } from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <ThemeProvider>
+    <SidebarProvider>
+     <MainContent />
+    </SidebarProvider>
+  </ThemeProvider>
   );
+}
+
+
+const MainContent = () => {
+  const {theme, toggleTheme} = useContext(ThemeContext);
+
+  return (
+    <div className={`app ${theme}`}>
+      <div className='main-content'>
+        <h1 className='cop-title'>Badr Bench</h1>
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/ctx/SidebarContext.js
@@ -0,0 +1,44 @@
+import React, { createContext, useState, useContext } from 'react';
+import {ThemeContext} from './ThemeContext';
+
+export const SidebarContext = createContext();
+
+const SidebarProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prevIsSidebarOpen) => !prevIsSidebarOpen);
+  };
+
+  return (
+    <SidebarContext.Provider value={{ isSidebarOpen, toggleSidebar }}>
+      <Sidebar/>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+
+const Sidebar = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+    const { isSidebarOpen, toggleSidebar } = useContext(SidebarContext);
+  
+    return (
+        <> 
+        <div onClick={toggleSidebar} className={`burger ${isSidebarOpen ? 'active' : ''}`}>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+      <aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+        <div className='sidebar-content'>
+            <div className='bottom-box'>
+            <div onClick={toggleTheme} className='theme-toggle'><a className='material-icons'>{theme!=='light' ? 'light_mode' : 'dark_mode'}</a></div>
+            </div>
+        </div>
+      </aside>
+      </>
+    );
+};  
+
+export default SidebarProvider;
